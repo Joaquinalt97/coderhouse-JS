6ec807777cf904fbd2b9daeb7b5ea2432aa2ef48
Joaquin Alatamirano <joaquinaltamirano70@gmail.com>
2025-08-28 21:04:23 -0300

    segunda pre entrega

--- app.js
+++ app.js
@@ -1,55 +1,80 @@
-alert ("hola, bienvenido a la 1er pre entrega de JavaScript. funcionare como tu carrito de compras, almacenare tus datos con tus productos.");
+alert("hola, en esta pre entrega n°2, intentare ser tu gestor de productos");
 
-let nombre
-nombre = prompt("ingresa tu nombre");
+alert("antes que nada te solicitare tu nombre");
 
-let edad
-edad = parseInt(prompt("ingresa tu edad"));
-
-let dinero 
-dinero = parseInt(prompt("ingresa tu cantidad de dinero, para realizar el calculo al final de tu cuenta debes ingresar un minimo de 100"));
-
-if (edad >= 12 && dinero >= 100) {
-    alert ("podemos dejarte ingresar al super");
-} else if (edad >= 12 || dinero <= 99) {
-    alert ("puedes pasar pero no tienes dinero, no puedes comprar mucho");
-} else if (edad <= 11 || dinero >= 100) {
-    alert ("eres menor, pero tienes dinero. esperaremos a que llegue un adulto");
-} else {
-    alert ("eres menor y ademas no tienes dinero. no podras pasar esperaremos a un adulto");
-} 
-
-alert ("bienvenido al supermercado");
-
-const IVA = 0.21;
-
-let totalDeArticulos = `articulos: \n`;
-let precioTotalSinIVA = 0;
-let agregar = confirm ("agrega los productos a añadir a tu lista");
-
-while (agregar) {
-    let nombreDelProducto = parseInt(prompt("ingresa el nombre del producto"));
-    let precioDelProducto;
-
-do {
-    precioDelProducto = parseInt(prompt("ingresa el precio del producto"));
-
-    if (isNaN(precioDelProducto) || precioDelProducto <= 0) {
-        alert("ingresa un numero, positivo.");
-    }
-
-} while(isNaN(precioDelProducto) || precioDelProducto <= 0);
-
-    precioTotalSinIVA += precioDelProducto;
-    totalDeArticulos += `${nombreDelProducto}\n`;
-agregar = confirm("¿quieres agregar otro arculo?");
+function saludar() {
+    return
 }
 
-let precioTotalConIVA = precioTotalSinIVA * (1 + IVA);
+let nombre = prompt("ingresa tu nombre");
 
-alert (`${totalDeArticulos}\n 
-    - precio total con iva = ${precioTotalConIVA}\n
-    - precio total sin iva = ${precioTotalSinIVA}\n
-    - tu dinero ${dinero}\n`);
+localStorage.setItem(`nombre`, nombre);
+saludar = `hola ${nombre} bienvenido!!`;
+alert(saludar);
 
-    document.write (`esta seria tu lista para imprimir ${totalDeArticulos}`);
+let clave = prompt("genera una clave provisoria");
+
+let claveUsuario = nombre + clave;
+
+sessionStorage.setItem(`Usuario`, claveUsuario);
+sessionStorage.removeItem(`claveUsuario`);
+
+let nombreIngresado = nombre;
+let claveIngresada = clave;
+
+
+let products = [];
+let produtId = 1;
+
+const form = document.getElementById("productForm");
+const container = document.getElementById("productsContainer");
+
+form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("productName").value;
+    const price = document.getElementById("productPrice").value;
+    const description = document.getElementById("productDescription").value;
+
+    const product = {
+        id: produtId++,
+        name: name,
+        price: price,
+        description: description,
+    };
+
+    localStorage.setItem(`productInfo`, JSON.stringify(product));
+
+    products.push(product);
+    renderProduts();
+    form.reset();
+});
+
+function renderProduts() {
+
+    container.innerHTML = "";
+    products.forEach((product) => {
+        const productDiv = document.createElement("div");
+        const productInfo = document.createElement("div");
+
+        productInfo.innerHTML = `${product.name} - $${product.price} - ${product.description}`;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Eliminar";
+
+        deleteButton.addEventListener("click", () => deleteProduct(product.id));
+
+        productDiv.appendChild(productInfo);
+        productDiv.appendChild(deleteButton);
+
+        container.appendChild(productDiv);
+    });
+}
+function deleteProduct(id) {
+    products = products.filter((product) => product.id !== id);
+    renderProduts();
+
+    localStorage.removeItem(`productInfo`);
+}
+
